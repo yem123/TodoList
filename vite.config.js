de9965997179da1b358e95b9a02187167082deb4
yemane <yemanemeasho2021@gmail.com>
2025-01-01 20:13:40 +0100

    add files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure the build output directory is set to "dist"
+  },
 });
